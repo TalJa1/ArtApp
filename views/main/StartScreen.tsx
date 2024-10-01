6eac3d6ba74bb9502374e926e29af4d9a552392f
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-10-01 14:04:22 +0700

    Refactor StartScreen component to add header with navigation button

--- views/main/StartScreen.tsx
+++ views/main/StartScreen.tsx
@@ -1,18 +1,28 @@
-import {ScrollView, StyleSheet, Text, View} from 'react-native';
+import {ScrollView, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import React from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import useStatusBar from '../../services/useStatusBarCustom';
+import {vw} from '../../services/styleSheets';
 
 const StartScreen = () => {
   useStatusBar('#C8E589');
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
+        <Header />
         <View>
           <Text>StartScreen</Text>
         </View>
       </ScrollView>
     </SafeAreaView>
+  );
+};
+
+const Header: React.FC = () => {
+  return (
+    <View style={styles.headerContainer}>
+      <TouchableOpacity></TouchableOpacity>
+    </View>
   );
 };
 
@@ -22,5 +32,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#C8E589',
+  },
+  headerContainer: {
+    paddingHorizontal: vw(5),
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
 });
