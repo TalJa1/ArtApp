67b675f54025c152072a5ffabd7e861ae8b9de4d
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-10-02 10:27:07 +0700

    Refactor StartScreen component to add StarGroupComponent for displaying star count

---
+++ components/main/StarGroupComponent.tsx
@@ -0,0 +1,48 @@
+import {Image, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {vh, vw} from '../../services/styleSheets';
+
+const StarGroupComponent = () => {
+  return (
+    <View style={styles.starContainer}>
+      <Image
+        style={styles.starImg}
+        source={require('../../assets/start/starCircle.png')}
+      />
+      <View style={styles.headerTxtContainer}>
+        <Text style={styles.headerTxt}>2000</Text>
+      </View>
+    </View>
+  );
+};
+
+export default StarGroupComponent;
+
+const styles = StyleSheet.create({
+  starContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  starImg: {
+    width: vw(12),
+    height: vw(12),
+    resizeMode: 'contain',
+    position: 'relative',
+    left: vw(4.5),
+    zIndex: 2,
+  },
+  headerTxtContainer: {
+    backgroundColor: '#EFBB00',
+    paddingVertical: vh(0.3),
+    paddingHorizontal: vw(5),
+    borderTopRightRadius: vw(20),
+    borderBottomRightRadius: vw(20),
+    borderWidth: 4,
+    borderColor: '#C8E589',
+  },
+  headerTxt: {
+    fontSize: 20,
+    color: '#fff',
+    fontWeight: '900',
+  },
+});
--- views/main/StartScreen.tsx
+++ views/main/StartScreen.tsx
@@ -13,6 +13,7 @@ import useStatusBar from '../../services/useStatusBarCustom';
 import {centerAll, vh, vw} from '../../services/styleSheets';
 import {grassIcon, playStartIcon} from '../../assets/svgXml';
 import {FourBtn} from '../../services/renderData';
+import StarGroupComponent from '../../components/main/StarGroupComponent';
 
 const generateRandomGrassIcons = (numIcons: number) => {
   return Array.from({length: numIcons}).map(() => {
@@ -132,15 +133,7 @@ const Header: React.FC = () => {
           source={require('../../assets/start/settingBtn.png')}
         />
       </TouchableOpacity>
-      <View style={styles.starContainer}>
-        <Image
-          style={styles.starImg}
-          source={require('../../assets/start/starCircle.png')}
-        />
-        <View style={styles.headerTxtContainer}>
-          <Text style={styles.headerTxt}>2000</Text>
-        </View>
-      </View>
+      <StarGroupComponent />
       <TouchableOpacity>
         <Image
           style={styles.headerBtnImg}
@@ -169,32 +162,6 @@ const styles = StyleSheet.create({
     width: vw(14),
     height: vw(14),
     resizeMode: 'contain',
-  },
-  starContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  starImg: {
-    width: vw(12),
-    height: vw(12),
-    resizeMode: 'contain',
-    position: 'relative',
-    left: vw(4.5),
-    zIndex: 2,
-  },
-  headerTxtContainer: {
-    backgroundColor: '#EFBB00',
-    paddingVertical: vh(0.3),
-    paddingHorizontal: vw(5),
-    borderTopRightRadius: vw(20),
-    borderBottomRightRadius: vw(20),
-    borderWidth: 4,
-    borderColor: '#C8E589',
-  },
-  headerTxt: {
-    fontSize: 20,
-    color: '#fff',
-    fontWeight: '900',
   },
   goOnBtn: {
     flexDirection: 'row',
