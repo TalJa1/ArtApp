e07c355b055a3361dccf00fce790336381ab03e9
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-10-02 11:11:08 +0700

    Refactor StartScreen component to add brush icons and update styles

--- services/renderData.tsx
+++ services/renderData.tsx
@@ -1,6 +1,25 @@
+import {
+  brush1Icon,
+  brush2Icon,
+  brush3Icon,
+  brush4Icon,
+  brush5Icon,
+  brush6Icon,
+} from '../assets/svgXml';
+import {vw} from './styleSheets';
+
 export const FourBtn = [
   require('../assets/start/shoppingBtn.png'),
   require('../assets/start/multiBtn.png'),
   require('../assets/start/cartBtn.png'),
   require('../assets/start/penListBtn.png'),
+];
+
+export const BrushList = [
+  brush1Icon(vw(12), vw(12)),
+  brush2Icon(vw(12), vw(12)),
+  brush3Icon(vw(12), vw(12)),
+  brush4Icon(vw(12), vw(12)),
+  brush5Icon(vw(12), vw(12)),
+  brush6Icon(vw(12), vw(12)),
 ];
--- views/main/StartScreen.tsx
+++ views/main/StartScreen.tsx
@@ -13,7 +13,7 @@ import {SafeAreaView} from 'react-native-safe-area-context';
 import useStatusBar from '../../services/useStatusBarCustom';
 import {centerAll, vh, vw} from '../../services/styleSheets';
 import {grassIcon, playStartIcon} from '../../assets/svgXml';
-import {FourBtn} from '../../services/renderData';
+import {BrushList, FourBtn} from '../../services/renderData';
 import StarGroupComponent from '../../components/main/StarGroupComponent';
 
 const generateRandomGrassIcons = (numIcons: number) => {
@@ -118,6 +118,21 @@ const Footer: React.FC = () => {
               style={styles.penlistIconImg}
               source={require('../../assets/start/penlistIcon.png')}
             />
+            <View style={styles.brushContainer}>
+              {BrushList.map((brush, index) => (
+                <TouchableOpacity
+                  key={index}
+                  disabled={index === 0 || index === 1 ? false : true}
+                  style={[
+                    styles.brushButton,
+                    index === 0 || index === 1
+                      ? styles.activeBrush
+                      : styles.inactiveBrush,
+                  ]}>
+                  {brush}
+                </TouchableOpacity>
+              ))}
+            </View>
             <TouchableOpacity
               style={styles.closeButton}
               onPress={() => setModalVisible(!modalVisible)}>
@@ -246,11 +261,13 @@ const styles = StyleSheet.create({
     borderWidth: 3,
     borderColor: 'black',
     paddingVertical: vh(7),
+    justifyContent: 'space-between',
   },
   closeButton: {
+    alignItems: 'center',
     backgroundColor: '#8ACE5D',
     paddingVertical: vh(0.6),
-    paddingHorizontal: vw(8),
+    width: '60%',
     borderRadius: 20,
   },
   closeButtonText: {
@@ -265,5 +282,24 @@ const styles = StyleSheet.create({
     resizeMode: 'cover',
     position: 'absolute',
     top: vh(-4),
+  },
+  brushContainer: {
+    paddingHorizontal: vw(5),
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-evenly',
+    width: '100%',
+    rowGap: vh(2),
+  },
+  brushButton: {
+    padding: vw(4),
+    borderRadius: vw(5),
+  },
+  activeBrush: {
+    backgroundColor: '#C8E589',
+  },
+  inactiveBrush: {
+    backgroundColor: '#3A3A3A',
+    opacity: 0.5,
   },
 });
