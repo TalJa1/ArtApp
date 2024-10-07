c818fe97741969995c145d1f7061f50d9f0a3b01
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-10-07 10:57:08 +0700

    Refactor Sketh.tsx to add footer component and update styling

--- views/main/Sketh.tsx
+++ views/main/Sketh.tsx
@@ -26,8 +26,25 @@ const Sketh = () => {
           <Header />
           <Main />
         </View>
+        <Footer />
       </ScrollView>
     </SafeAreaView>
+  );
+};
+
+const Footer: React.FC = () => {
+  return (
+    <View style={{flexDirection: 'row'}}>
+      <Image
+        style={[styles.footerImg, styles.footerImg1]}
+        source={require('../../assets/mainSketh/footer1.png')}
+      />
+      <Image
+        style={[styles.footerImg, styles.footerImg2]}
+        source={require('../../assets/mainSketh/footer2.png')}
+      />
+      <Image style={styles.icon1} source={require('../../assets/mainSketh/icon1.png')}/>
+    </View>
   );
 };
 
@@ -116,4 +133,17 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: 'white',
   },
+  footerImg: {
+    width: vw(100),
+    height: vh(15),
+    resizeMode: 'cover',
+  },
+  icon1:{
+    zIndex: 3,
+    position: 'absolute',
+    bottom: vh(3),
+    right: vw(3),
+  },
+  footerImg1: {zIndex: 2},
+  footerImg2: {zIndex: 1, position: 'absolute', bottom: 0},
 });
