0342a3fd0f9e601f94c18097a6d0ee2302fca904
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-10-08 10:41:55 +0700

    Refactor DrawScreen component to add header with back button and star group

--- views/draw/DrawScreen.tsx
+++ views/draw/DrawScreen.tsx
@@ -1,9 +1,20 @@
-import {ScrollView, StyleSheet, Text, View} from 'react-native';
+/* eslint-disable react-native/no-inline-styles */
+import {
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import React from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import useStatusBar from '../../services/useStatusBarCustom';
-import {RouteProp, useRoute} from '@react-navigation/native';
+import {RouteProp, useNavigation, useRoute} from '@react-navigation/native';
 import {DetailRouteParams} from '../../services/typeProps';
+import StarGroupComponent from '../../components/main/StarGroupComponent';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import {vh, vw} from '../../services/styleSheets';
 
 const DrawScreen = () => {
   useStatusBar('white');
@@ -14,12 +25,43 @@ const DrawScreen = () => {
 
   return (
     <SafeAreaView style={styles.container}>
-      <ScrollView>
-        <View>
-          <Text>DrawScreen</Text>
+      <ScrollView contentContainerStyle={{flexGrow: 1}}>
+        <View style={{flex: 1, marginBottom: vh(2), paddingHorizontal: vw(5)}}>
+          <Header />
+          <View style={styles.iconGroup}>
+            <View style={styles.iconGroupImgContainer}>
+              <Image
+                style={styles.iconGroupImg}
+                source={require('../../assets/mainSketh/drawIcon.png')}
+              />
+            </View>
+            <View style={styles.iconGroupTxtContainer}>
+              <Text style={styles.iconGroupTxt}>Hãy vẽ {data.title}</Text>
+            </View>
+          </View>
         </View>
       </ScrollView>
     </SafeAreaView>
+  );
+};
+
+const Header: React.FC = () => {
+  const navigation = useNavigation<NativeStackNavigationProp<any>>();
+
+  return (
+    <View style={styles.headerContainer}>
+      <TouchableOpacity onPress={() => navigation.goBack()}>
+        <Image source={require('../../assets/mainSketh/backBtn.png')} />
+      </TouchableOpacity>
+      <StarGroupComponent
+        starCount={2000}
+        color="#EFBB00"
+        borderColor="#FEF9BD"
+      />
+      <TouchableOpacity disabled>
+        <Image source={require('../../assets/mainSketh/settingBtn.png')} />
+      </TouchableOpacity>
+    </View>
   );
 };
 
@@ -29,5 +71,43 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'white',
+  },
+  headerContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginVertical: vh(2),
+  },
+  iconGroup: {
+    flexDirection: 'row',
+    alignSelf: 'center',
+    alignItems: 'center',
+  },
+  iconGroupImgContainer:{
+    zIndex: 2,
+    position:'relative',
+    left: vw(9),
+  },
+  iconGroupImg: {
+    width: vw(20),
+    height: vw(20),
+    resizeMode: 'contain',
+  },
+  iconGroupTxtContainer: {
+    backgroundColor: '#E5F0FE',
+    paddingVertical: vh(1.5),
+    paddingHorizontal: vw(10),
+    borderRadius: vw(25),
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    // Elevation for Android
+    elevation: 5,
+  },
+  iconGroupTxt: {
+    fontSize: 20,
+    color: '#000000',
+    fontWeight: 'bold',
   },
 });
