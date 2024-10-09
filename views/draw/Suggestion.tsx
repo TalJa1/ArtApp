ee7baaa516129e5f32b254983a6fb4ef88070be4
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-10-09 14:41:42 +0700

    Refactor Suggestion screen to include coins prop and handle coin data

--- views/draw/Suggestion.tsx
+++ views/draw/Suggestion.tsx
@@ -1,17 +1,40 @@
+/* eslint-disable react-native/no-inline-styles */
 import {ScrollView, StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, {useCallback, useState} from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
-import {RouteProp, useRoute} from '@react-navigation/native';
+import {RouteProp, useFocusEffect, useRoute} from '@react-navigation/native';
 import {SuggestionProps} from '../../services/typeProps';
+import {loadData, saveData} from '../../services/storage';
+import HeaderSketch from '../../components/HeaderSketch';
+import {vw} from '../../services/styleSheets';
 
 const Suggestion = () => {
   const route = useRoute<RouteProp<SuggestionProps, 'SuggestionItem'>>();
   const {imgIndex} = route.params;
+  const [coins, setCoins] = useState(0);
+
+  const fetchData = async () => {
+    await loadData<number>('CoinsStorage')
+      .then(dataCoins => {
+        setCoins(dataCoins);
+      })
+      .catch(() => {
+        setCoins(2000);
+        saveData('CoinsStorage', 2000);
+      });
+  };
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchData();
+    }, []),
+  );
 
   return (
     <SafeAreaView style={styles.container}>
-      <ScrollView>
-        <View>
+      <ScrollView contentContainerStyle={{flexGrow: 1}}>
+        <View style={{paddingHorizontal: vw(5)}}>
+          <HeaderSketch coins={coins} />
           <Text>Suggestion</Text>
         </View>
       </ScrollView>
