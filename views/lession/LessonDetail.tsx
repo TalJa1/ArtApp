cffa0a3cdd704c6bbb98f92da01b2938ae0dc7fc
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-10-10 16:45:40 +0700

    Refactor LessonDetail component and add BackBtn

--- views/lession/LessonDetail.tsx
+++ views/lession/LessonDetail.tsx
@@ -1,16 +1,21 @@
+/* eslint-disable react-native/no-inline-styles */
 import {ScrollView, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import useStatusBar from '../../services/useStatusBarCustom';
+import {vh, vw} from '../../services/styleSheets';
+import BackBtn from '../../components/extra/BackBtn';
 
 const LessonDetail = () => {
-  useStatusBar('#7CBFF9');
+  useStatusBar('#899AF8');
 
   return (
     <SafeAreaView style={styles.container}>
-      <ScrollView>
-        <View>
-          <Text>LessonDetail</Text>
+      <ScrollView contentContainerStyle={{flexGrow: 1}}>
+        <View style={{flex: 1, paddingHorizontal: vw(5)}}>
+          <View style={{marginVertical: vh(2)}}>
+            <BackBtn />
+          </View>
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -20,5 +25,5 @@ const LessonDetail = () => {
 export default LessonDetail;
 
 const styles = StyleSheet.create({
-  container: {flex: 1, backgroundColor: '#7CBFF9'},
+  container: {flex: 1, backgroundColor: '#899AF8'},
 });
