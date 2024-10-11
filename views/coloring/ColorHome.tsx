1ab31d9c73ce851b2182b3c2921773453bcf8b69
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-10-11 09:49:05 +0700

    Refactor ColorHome to add header component

--- views/coloring/ColorHome.tsx
+++ views/coloring/ColorHome.tsx
@@ -1,15 +1,21 @@
-import {ScrollView, StyleSheet, Text, View} from 'react-native';
+/* eslint-disable react-native/no-inline-styles */
+import {ScrollView, StyleSheet, View} from 'react-native';
 import React from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import useStatusBar from '../../services/useStatusBarCustom';
+import Header from '../../components/extra/Header';
+import {vw} from '../../services/styleSheets';
 
 const ColorHome = () => {
   useStatusBar('#EF99DA');
   return (
     <SafeAreaView style={styles.container}>
-      <ScrollView>
-        <View>
-          <Text>ColorHome</Text>
+      <ScrollView contentContainerStyle={{flexGrow: 1}}>
+        <View style={{paddingHorizontal: vw(5)}}>
+          <Header
+            img={require('../../assets/color/icon1.png')}
+            title="Tô màu"
+          />
         </View>
       </ScrollView>
     </SafeAreaView>
